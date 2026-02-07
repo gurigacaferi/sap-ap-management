cd66305513eb5c7dd67e9fc5e720372387bf5b18
[dyad] <gurigaca12@gmail.com>
2026-02-07 13:08:25 +0200

    [dyad] Adding automatic scroll-to-top on page navigation - wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Contact from "./pages/Contact";
 import Process from "./pages/Process";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/contact" element={<Contact />} />
